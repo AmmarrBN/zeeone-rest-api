7eb5aba710f94b7cce3a101e8a53b8d33fea5bf5
AmmarXploit <104218064+AmmarrBN@users.noreply.github.com>
2023-12-25 21:28:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 global.creator = 'AmmarBN'// yourname
 global.MONGO_DB_URI = "mongodb+srv://AmmarBN:VzmTxPWPuJysywAC@cluster0.uxut7jf.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "akuntumbal8836@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
